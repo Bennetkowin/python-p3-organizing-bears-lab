a3dffb6fe24f22530154942f6f38fbfec28428d3
Bennet Owino <bennetkowin09@gmail.com>
2025-03-09 03:27:30 +0300

    done

--- lib/sql_queries.py
+++ lib/sql_queries.py
@@ -1,18 +1,25 @@
+# Select all female bears with their name and age
 select_all_female_bears_return_name_and_age = """
-    Write your SQL query here
+    SELECT name, age FROM bears WHERE sex = 'F';
 """
 
+# Select all bears' names in alphabetical order
 select_all_bears_names_and_orders_in_alphabetical_order = """
-    Write your SQL query here
+    SELECT name FROM bears ORDER BY name ASC;
 """
 
-select_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest = """
-    Write your SQL query here
+# Select all bears that are alive
+select_all_alive_bears = """
+    SELECT * FROM bears WHERE alive = 1;
 """
 
-select_oldest_bear_and_returns_name_and_age = """
-    Write your SQL query here
+# Select the oldest bear
+select_oldest_bear = """
+    SELECT * FROM bears ORDER BY age DESC LIMIT 1;
 """
-select_youngest_bear_and_returns_name_and_age = """
-    Write your SQL query here
+
+# Select the youngest bear
+select_youngest_bear = """
+    SELECT * FROM bears ORDER BY age ASC LIMIT 1;
 """
+Now your test should be able to find the expected query. Try running your tests again! 🚀
